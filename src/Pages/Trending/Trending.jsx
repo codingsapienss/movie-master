74565857ee967884fd063826c454d4fcd1560d29
codingsapienss <contactprsant@gmail.com>
2023-12-10 08:42:38 +0530

    UI Update

--- src/Pages/Trending/Trending.jsx
+++ src/Pages/Trending/Trending.jsx
@@ -15,7 +15,7 @@ const Trending = () => {
   const fetchTrending = async () => {
     const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=fa3cc0a92ba7aff7d30336ebc415def7&page=${page}`)
 
-    console.log(data)
+    // console.log(data)
     setContent(data.results)
     setTotalPages(data.total_pages)
   }
