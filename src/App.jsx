b55e9720c8384b47e785f39239f60cc64f96a2d7
codingsapienss <contactprsant@gmail.com>
2023-12-18 10:44:06 +0530

    UI Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import "./App.css"
-import BottomNav from "./Components/BottomNav/BottomNav"
-import Header from './Components/Header/Header'
-import Trending from "./Pages/Trending/Trending"
-import Movies from "./Pages/Movies/Movies"
-import Series from "./Pages/Series/Series"
-import Search from "./Pages/Search/Search"
-
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import BottomNav from "./Components/BottomNav/BottomNav";
+import Header from "./Components/Header/Header";
+import Trending from "./Pages/Trending/Trending";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Search from "./Pages/Search/Search";
 
 const App = () => {
   return (
     <div className="app">
-      {/* <Header/> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Trending />} />
         <Route path="/movies" element={<Movies />} />
@@ -22,7 +20,7 @@ const App = () => {
 
       <BottomNav />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
